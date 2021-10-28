404bfbea44f4f26586e135f682efabe3e4156026
MarianaBI <ignaciomariana03@gmail.com>
2021-10-28 19:32:55 -0300

    exercicio javascript

--- exercicio aula 21/main.js
+++ exercicio aula 21/main.js
@@ -1,2 +1,9 @@
 // Exercicio
-console.clear();
+let nome = 'Mari'
+let peso = 50
+let altura = 1.58
+let imc = altura * altura / peso
+
+const hello =  'tem de altura ' + altura + ' e peso ' + peso + ', seu IMC é ' + imc
+
+console.log (hello);
