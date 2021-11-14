2489dfb4164936bfa7470fefa10b62a5bfefda94
MarianaBI <ignaciomariana03@gmail.com>
2021-11-13 21:07:42 -0300

    aula24.exercicio

---
+++ aula24.estrutura condicional/main.js
@@ -0,0 +1,24 @@
+const  a  =  10 ;
+if  ( a  ==  10 )  {
+    console . log ( "Os valores são iguais" ) ;
+} else  if  ( a  >  10 ) {
+console . log ( "A variavel A não é igual a 10 e não é maior que 10" )
+}
+
+//TROCAR
+const  nota  =  6 ;
+
+switch ( nota )  {
+  caso  4 :
+    console . log ( "Reprovado!" )
+    pausa ;
+  caso  6 :
+    console . log ( "Recuperação" ) ;
+    pausa ;
+    padrão :
+        console . log ( "Final" ) ;
+    pausa ;  
+}
+
+const  c  =  nota  ==  4 ? “Reprovado” : “Recuperação” ;
+console . log  ( c ) 
